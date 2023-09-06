050f5f3de26e3534eb4863b702faa52bdd78e1fe
rebeizantoine <rebeizantoine@gmail.com>
2023-09-06 11:44:16 +0300

    Antoine react

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "369f007b74msh37d8265202a7ce5p1a6344jsnf4481e887526",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "210720a15b472c5f5f50dd87103edfe3"; // enter your key from openweather API
