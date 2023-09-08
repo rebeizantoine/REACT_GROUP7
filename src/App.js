dda5de9b910d279292e81c97c11ad671752cb876
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-09-08 11:36:03 +0300

    adding css and time function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import pic from "./components/img/House.png";
+import React, { useState } from "react";
+import DateTimeDisplay from "./dateTime.js";
+
+const api = {
+  key: "61af75f3f18c7bf8d2b68a365c65c688",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+  const [forecast, setForecast] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+
+  const searchPressed = () => {
+    if (search.trim() === "") {
+      setErrorMessage("Please enter a city name.");
+      return;
+    } else {
+      setErrorMessage("");
+    }
+
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+
+    fetch(`${api.base}forecast?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        const forecastData = result?.list;
+        if (forecastData) {
+          const filteredForecast = forecastData.slice(0, 4);
+          setForecast(filteredForecast);
+        } else {
+          setErrorMessage("Error fetching forecast data.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching forecast data:", error);
+      });
+  };
+
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Tech Chuckle-trek</h1>
       </header>
+
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Enter city name "
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button onClick={searchPressed}>Search</button>
+      </div>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      {typeof weather.main !== "undefined" && (
+        <div className="sub-title">
+          {/* Location  */}
+          <p>{weather.name}</p>
+          {/* Temperature Celsius  */}
+          <p>{weather.main.temp}°C</p>
+          {/* humidity */}
+          <p> {weather.main.humidity !== undefined ? `${weather.main.humidity}%` : "N/A"}</p>
+          {/* Condition (Sunny ) */}
+          <p>{weather.weather[0].main}</p>
+        </div>
+      )}
+      <div className="dateTime">
+        {/* render from another file */}
+        <DateTimeDisplay />
+      </div>
+
+      <div className="image">
+        <img src={pic} alt="Description of the image" />
+      </div>
+
+      <div className="wrapper">
+
+        {/* Display weather forecast */}
+
+        {forecast.length >= 0 && (
+          <div className="weather-rect">
+            {forecast.slice(0, 4).map((forecastItem, index) => (
+              <div key={index} className="mini-rec">
+                {/* Display forecast data here */}
+                {/* <h2>{index + 1}</h2> */}
+                <p> date {new Date(forecastItem.dt_txt).getDate()}</p>
+                <img src={`https://openweathermap.org/img/wn/${forecastItem.weather[0].icon}.png`} alt="icon" />
+                <p> {forecastItem.main.temp}°C</p>
+                <p> {forecastItem.weather[0].main}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/dateTime.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+function formatDateAndTime(date) {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const dayOfMonth = date.getDate();
+    const month = months[date.getMonth()];
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+
+    return `${dayOfWeek} ${dayOfMonth} ${month}  | ${formattedHours}: ${minutes} ${ampm}`;
+}
+
+function DateTimeDisplay() {
+    const [formattedDateAndTime, setFormattedDateAndTime] = useState("");
+
+    useEffect(() => {
+        const currentDate = new Date(); // Get the current date and time
+        const formattedDateTime = formatDateAndTime(currentDate);
+        setFormattedDateAndTime(formattedDateTime);
+    }, []);
+
+    return <div>{formattedDateAndTime}</div>;
+}
+
+export default DateTimeDisplay;
